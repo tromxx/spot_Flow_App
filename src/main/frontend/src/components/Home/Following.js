cfd4266e616984467247aea473b4507c12ed9b8b
husik927@gmail.com <husik927@gmail.com>
2023-09-21 22:39:22 +0900

    singup fix

---
+++ src/main/frontend/src/components/Home/Following.js
@@ -0,0 +1,205 @@
+import styled from "styled-components";
+import { AiOutlineClose } from 'react-icons/ai'
+import {MdOutlineArrowBack} from 'react-icons/md'
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../context/UserStore';
+import Error from '../Common/Error';
+import FollowApi from "../../api/FollowApi";
+
+const FollowDiv = styled.div`
+    width: 390px;
+    height: calc(100vh - 7vh - 3px);
+    transition: all 1s;
+    display: flex;
+    flex-direction: column;
+    justify-content: baseline;
+    align-items: center;
+    position: absolute;
+    background-color: ${props => props.theme.bgColor};
+    color: ${props => props.theme.textColor};
+    border: ${props => props.theme.borderColor};
+    left: ${(props) => (props.$active ? '0' : '-390px')};
+    z-index: 3;
+    .controlDiv {
+        margin-top: 15px;
+        display: flex;
+        gap: 250px;
+    }
+    .followCounter{
+        margin-top: 25px;
+        display: flex;
+        font-size: large;
+        font-weight: bolder;
+        gap: 100px;
+        p{
+            cursor: pointer;
+            &:hover{
+                color: var(--lightblue);
+            }
+        }
+    }
+    .FollowingErrorContainer{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .FollowingContainer{
+        width: 350px;
+        height: 75vh;
+        margin-top: 25px;
+        overflow-y: scroll;
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        &::-webkit-scrollbar {
+            width: 8px;
+            height: 8px;
+            border-radius: 6px;
+            background: rgba(255, 255, 255, 0.4);
+        }
+        &::-webkit-scrollbar-thumb {
+            background: rgba(0, 0, 0, 0.3);
+            border-radius: 6px;
+        }
+        .userContainer{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 30px;
+            padding: 10px;
+            border-radius: 30px;
+            font-weight: bolder;
+            border: ${props => props.theme.borderColor};
+            img{
+                width: 50px;
+                height: 50px;
+                border-radius: 50px;
+                border: 1px solid var(--grey);
+            }
+            button{
+                width: 150px;
+                height: 50px;
+                background-color: var(--lightblue);
+                color: ${props => props.theme.textColor};
+                border: none;
+                font-weight: bolder;
+                border-radius: 20px;
+                cursor: pointer;
+                &:hover{
+                   color: var(--blue);
+                }
+            }
+            p{
+                width: 100px;
+                cursor: pointer;
+                overflow-wrap: break-word;
+    	        word-wrap: break-word;
+    	        word-break: break-all;
+                &:hover{
+                    color: var(--lightblue);
+                }
+            }
+        }
+    }
+  	@media (max-width : 844px){
+    	height: 100vh;
+  	}
+`;
+
+const CloseButton = styled(AiOutlineClose)`
+    width: 35px;
+    height: 35px;
+    color: var(--grey);
+    cursor: pointer;
+    &:hover {
+        cursor: pointer;
+        color: var(--lightblue);
+    }
+`;
+
+const BackButton = styled(MdOutlineArrowBack)`
+    width: 35px;
+    height: 35px;
+    color: var(--grey);
+    cursor: pointer;
+    &:hover {
+        cursor: pointer;
+        color: var(--lightblue);
+    }
+`;
+
+const LogOutDiv = styled.div`
+    width: 390px;
+    height: calc(100vh - 7vh - 1px);
+    transition: all 1.7s;
+    position: absolute;
+    background-color: white;
+    left: ${(props) => (props.$active ? '0' : '-390px')};
+    z-index: 3;
+    @media (max-width: 844px) {
+        height: 100vh;
+    }
+`;
+
+
+
+const Following = ({ active, handleActive, handlePage, handleFollower }) => {
+    const { isLoggedIn, follower, following, setFollowing } = useContext(UserContext);
+    const [data, setData] = useState([]);
+
+    useEffect(()=>{
+        const getFollowing = async() => {
+            const response = await FollowApi.getFollowing();
+            console.log(response.data);
+            setData(response.data);
+        }
+        getFollowing();
+    },[])
+
+    const delFollowing = async(e) => {
+        const response = await FollowApi.delFollowing(e);
+        if(response){
+            setData(prevData => prevData.filter(item => item.id !== e));
+            setFollowing(response.data)
+        }
+    }
+
+    return (
+        <>
+            {isLoggedIn ? (
+                <FollowDiv $active={active}>
+                    <div className="controlDiv">
+                        <BackButton onClick={handlePage} />
+                        <CloseButton onClick={handleActive} />
+                    </div>
+                    <div className="followCounter">
+                        <p onClick={handleFollower}>follower : {follower}</p>
+                        <p>following : {following}</p>
+                    </div>
+                    <div className="FollowingContainer">
+                        {data.map((datas) => (
+                            <div key={datas.id} className="userContainer">
+                                <p><img src={datas.url} alt="" /></p>
+                                <p>{datas.nickname}</p>
+                                <button onClick={()=>delFollowing(datas.id)}>UnFollowing</button>
+                            </div>
+                        ))}
+                        {data.length === 0 && (
+                            <div className="FollowingErrorContainer">
+                                <p>팔로잉 중인 사람이 없습니다.</p>
+                            </div>
+                        )}
+                    </div>
+                </FollowDiv>
+            ) : (
+                <LogOutDiv $active={active}>
+                    <Error />
+                </LogOutDiv>
+            )}
+        </>
+    );
+
+};
+
+export default Following;
--- src/main/frontend/src/context/UserStore.js
+++ src/main/frontend/src/context/UserStore.js
@@ -1,4 +1,5 @@
-import { createContext, useState }  from "react";
+import { createContext, useEffect, useState }  from "react";
+import CustomerApi from "../api/CustomerApi";
 export const UserContext = createContext(null);
 
 const UserStore = (props) => {
@@ -11,6 +12,30 @@ const UserStore = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [received, setReceived] = useState("");
   const [joinDate, setJoinDate] = useState("");
+  const token = localStorage.getItem('authToken');
+
+  useEffect(()=>{
+    const getCustomerInfo = async() => {
+      if(token != null){
+        try{
+          const response = await CustomerApi.getCustomerInfo(token);
+          setEmail(response.data.customer.email);
+          setNickname(response.data.customer.nickName);
+          setProfilePic(response.data.customer.profilePic);
+          setStatMsg(response.data.customer.statMsg);
+          setFollower(response.data.follower.follower);
+          setFollowing(response.data.follower.following);
+          setJoinDate(response.data.joinDate);
+          setIsLoggedIn(true);
+        }catch(error){
+          localStorage.clear();
+          setIsLoggedIn(false);
+        }
+      }
+    };
+    getCustomerInfo();
+  },[token])
+
 
 
   return (
